f1c0f42966ccf6822fac306a14c139ec2cd70e3b
scnuckc <99108901+scnuckc@users.noreply.github.com>
2023-01-11 00:24:56 +0800

    Add files via upload

---
+++ LibrarySystem/src/com/littleheap/Static/Information.java
@@ -0,0 +1,28 @@
+package com.littleheap.Static;
+
+import java.util.ArrayList;
+
+//import org.eclipse.ui.internal.wizards.NewWizardRegistry;
+
+import com.littleheap.Book;
+
+public class Information {
+
+	//数据库用户名
+	public static String username = "root";
+	//数据库密码
+	public static String password = "Scnu19";
+	//数据库URL地址
+	public static String JDBC_URL = "jdbc:mysql://192.168.229.135:3306/BookManageSYS";
+	//查询数据库图书类别名称
+	public static String search_classname = "";
+	//查询数据库图书列表
+	public static ArrayList<Book> bookarray = new ArrayList<Book>();
+	//当前管理员
+	public static String manager = "";
+	//当前用户
+	public static String user = "";
+	//查询Table列表
+	public static ArrayList<String> tablearray = new ArrayList<String>();
+
+}
